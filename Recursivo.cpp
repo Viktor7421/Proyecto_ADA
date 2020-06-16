0a53f16cc7f54458a93171103518ae8e0dbb9681
Viktor7421 <40173220+Viktor7421@users.noreply.github.com>
2020-06-16 17:21:03 -0500

    Add files via upload

--- Recursivo.cpp
+++ Recursivo.cpp
@@ -21,27 +21,63 @@ float Sumatoria(vector<pair<int,int>> X, int min, int max){
     return Suma;
 }
 
-pair<vector<pair <int, int>>, float> Min(pair<vector<pair <int, int>>, float> A, pair<vector<pair <int, int>>, float> B, pair<vector<pair <int, int>>, float> C){
+vector<pair<int,int>> Divisiones(vector<int> X){
+    vector<pair<int,int>> Divisiones;
+    for (int i = 0; i < X.size(); i++){
+        if(X[i] == 1){
+            int start = i;
+            while ( X[i+1] == 1 ){ i++;}
+            Divisiones.push_back({start,i});
+        }
+    }
+    return Divisiones;
+}
+
+vector<int> RestBinario(vector<int> X, int min, int max){
+    for(int i = min; i < max; i++){
+        X.pop_back();
+    }
+    return X;
+}
+
+pair<vector<pair <int, int>>, float> Min(pair<vector<pair <int, int>>, float> A, pair<vector<pair <int, int>>, float> B){
     pair<vector<pair <int, int>>, float> X;
     X.second = FLT_MAX;
     if(X.second >= A.second){X = A;}
     if(X.second >= B.second){X = B;}
-    if(X.second >= C.second){X = C;}
     return X;
 }
 
 
-pair<vector<pair <int, int>>, float> MinMatching(vector<pair<int,int>> A, vector<pair<int,int>> B){
+pair<vector<pair <int, int>>, float> MinMatching(vector<int> A, vector<int> B){
 
     //--------------------------------------------------------//
 
-    if(A.size() == 1){
-        float pesoB = Sumatoria(B);
-        float pesoA = (A[0].second-A[0].first+1);
+    vector<pair<int,int>> DivisionesA = Divisiones(A);
+    vector<pair<int,int>> DivisionesB = Divisiones(B);
+
+    //--------------------------------------------------------//
+
+    if(DivisionesA.size() == 1 && DivisionesB.size() == 1){
+        float pesoA = (DivisionesA[0].second-DivisionesA[0].first+1);
+        float pesoB = (DivisionesB[0].second-DivisionesB[0].first+1);
         float peso = pesoA/pesoB;
 
         vector<pair <int, int>> X;
-        for(int i = 0; i < B.size(); i++){
+        X.push_back({0,0});
+
+        return {X,peso};
+    }
+
+    //--------------------------------------------------------//
+
+    else if(DivisionesA.size() == 1){
+        float pesoB = Sumatoria(DivisionesB);
+        float pesoA = (DivisionesA[0].second-DivisionesA[0].first+1);
+        float peso = pesoA/pesoB;
+
+        vector<pair <int, int>> X;
+        for(int i = 0; i < DivisionesB.size(); i++){
             X.push_back({0,i});
         }
 
@@ -50,13 +86,13 @@ pair<vector<pair <int, int>>, float> MinMatching(vector<pair<int,int>> A, vector
 
     //--------------------------------------------------------//
 
-    if(B.size() == 1){
-        float pesoA = Sumatoria(A);
-        float pesoB = (B[0].second-B[0].first+1);
+    else if(DivisionesB.size() == 1){
+        float pesoA = Sumatoria(DivisionesA);
+        float pesoB = (DivisionesB[0].second-DivisionesB[0].first+1);
         float peso = pesoA/pesoB;
 
         vector<pair <int, int>> X;
-        for(int i = 0; i < A.size(); i++){
+        for(int i = 0; i < DivisionesA.size(); i++){
             X.push_back({i,0});
         }
 
@@ -65,70 +101,72 @@ pair<vector<pair <int, int>>, float> MinMatching(vector<pair<int,int>> A, vector
 
     //--------------------------------------------------------//
 
-    vector<pair <int, int>> Acopia = A;
-    vector<pair <int, int>> Bcopia = B;
+    vector<pair <int, int>> Acopia;
+    vector<pair <int, int>> Bcopia;
 
-    Acopia.pop_back();
+    //--------------------------------------------------------//
+
+    pair<vector<pair <int, int>>, float> peso1;
+
+    Acopia = DivisionesA;
+    Bcopia = DivisionesB;
+
+    peso1.second = FLT_MAX; 
+    
+    int index_1;
+
     Bcopia.pop_back();
-    pair <int, int> Ai = A.back();
-    pair <int, int> Bj = B.back();
-    float pesoAi = Ai.second - Ai.first + 1;
-    float pesoBj = Bj.second - Bj.first + 1;
-    pair<vector<pair <int, int>>, float> peso1 =  MinMatching(Acopia,Bcopia);
-    peso1.second = peso1.second + pesoAi/pesoBj;
+    float pesoB = DivisionesB.back().second-DivisionesB.back().first + 1;
+    for(int l = 0; l < DivisionesA.size()-1; l++){
+        float pesoA = Sumatoria(DivisionesA,DivisionesA.size()-l-1,DivisionesA.size());
+        Acopia.pop_back();
+        auto binarioA = RestBinario(A,Acopia.back().second+1,A.size());
+        auto binarioB = RestBinario(B,Bcopia.back().second+1,B.size());
+        auto OPT = MinMatching(binarioA,binarioB);
+        if(peso1.second > OPT.second+pesoA/pesoB){
+            peso1.second = OPT.second+pesoA/pesoB;
+            peso1.first =  OPT.first;
+            index_1 = l;
+        }
+    }
+
+    for(int k = DivisionesA.size()-index_1; k < DivisionesA.size();k++)
+            peso1.first.push_back({k-1,DivisionesB.size()-1});
 
     //--------------------------------------------------------//
 
     pair<vector<pair <int, int>>, float> peso2;
 
-    Acopia = A;
-    Bcopia = B;
+    Acopia = DivisionesA;
+    Bcopia = DivisionesB;
 
     peso2.second = FLT_MAX; 
     
+    int index_2;
+
     Acopia.pop_back();
     Bcopia.pop_back();
-    float pesoB = B.back().second-B.back().first + 1;
-    for(int i = 1; i < A.size()-1; i++){
-        float pesoA = Sumatoria(A,A.size()-i-1,A.size());
-        Acopia.pop_back();
-        auto OPT = MinMatching(Acopia,Bcopia);
+    float pesoA = DivisionesA.back().second-DivisionesA.back().first + 1;
+    for(int l = 1; l < DivisionesB.size()-1; l++){
+        float pesoB = Sumatoria(DivisionesB,DivisionesB.size()-l-1,DivisionesB.size());
+        Bcopia.pop_back();
+        auto binarioA = RestBinario(A,Acopia.back().second+1,A.size());
+        auto binarioB = RestBinario(B,Bcopia.back().second+1,B.size());
+        auto OPT = MinMatching(binarioA,binarioB);
         if(peso2.second > OPT.second+pesoA/pesoB){
             peso2.second = OPT.second+pesoA/pesoB;
             peso2.first =  OPT.first;
-            for(int j = A.size()-i; j < A.size();j++)
-                peso2.first.push_back({j-1,B.size()-1});
+            index_2 = l;
         }
     }
 
-    //--------------------------------------------------------//
-
-    pair<vector<pair <int, int>>, float> peso3;
-
-    Acopia = A;
-    Bcopia = B;
-
-    peso3.second = FLT_MAX; 
-    
-    Acopia.pop_back();
-    Bcopia.pop_back();
-    float pesoA = A.back().second-A.back().first + 1;
-    for(int i = 1; i < B.size()-1; i++){
-        float pesoB = Sumatoria(B,B.size()-i-1,B.size());
-        Bcopia.pop_back();
-        auto OPT = MinMatching(Acopia,Bcopia);
-        if(peso3.second > OPT.second+pesoA/pesoB){
-            peso3.second = OPT.second+pesoA/pesoB;
-            peso3.first =  OPT.first;
-            for(int j = B.size()-i; j < B.size();j++)
-                peso3.first.push_back({A.size()-1,j-1});
-        }
-    }
+    for(int k = DivisionesB.size()-index_2; k < DivisionesB.size();k++)
+        peso2.first.push_back({DivisionesA.size()-1,k-1});
 
     //--------------------------------------------------------//
 
-    pair<vector<pair <int, int>>, float> X = Min(peso1,peso2,peso3);
-    X.first.push_back({A.size()-1,B.size()-1});
+    pair<vector<pair <int, int>>, float> X = Min(peso1,peso2);
+    X.first.push_back({DivisionesA.size()-1,DivisionesB.size()-1});
 
     return X;
 }
@@ -136,45 +174,12 @@ pair<vector<pair <int, int>>, float> MinMatching(vector<pair<int,int>> A, vector
 
 int main( ) {
     vector<int> A = {1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0};
-    vector<int> B = {1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0};
-    vector<pair<int,int>> DivisionesA;
-    vector<pair<int,int>> DivisionesB;
-    int p = 20;
+    vector<int> B = {1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0};
 
-    //--------------------------------------------------------//
+    vector<pair<int,int>> DivisionesA = Divisiones(A);
+    vector<pair<int,int>> DivisionesB = Divisiones(B);
 
-    for (int i = 0; i < p; i++){
-        if(A[i] == 1){
-            int start = i;
-            while ( A[i+1] == 1 ){ i++;}
-            DivisionesA.push_back({start,i});
-        }
-    }
-
-    cout << "Divisiones A:\n";
-    for(auto i = DivisionesA.begin(); i != DivisionesA.end(); i++){
-        cout << '(' << (*i).first << ',' << (*i).second << ")\n";
-    }
-
-
-    //--------------------------------------------------------//
-
-    for (int i = 0; i < p; i++){
-        if(B[i] == 1){
-            int start = i;
-            while ( B[i+1] == 1 ){ i++;}
-            DivisionesB.push_back({start,i});
-        }
-    }
-
-    cout << "Divisiones B:\n";
-    for(auto i = DivisionesB.begin(); i != DivisionesB.end(); i++){
-        cout << '(' << (*i).first << ',' << (*i).second << ")\n";
-    }
-
-    //--------------------------------------------------------//
-
-    auto X = MinMatching(DivisionesA,DivisionesB);
+    auto X = MinMatching(A,B);
     for(auto i = X.first.begin(); i != X.first.end(); i++){
         cout << '(' << (*i).first << ',' << (*i).second << ")\n";
     }
